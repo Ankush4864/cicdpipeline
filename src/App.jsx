de88717915a88b323b792c977c347b6127502c4b
Ankush <ankush4864.be22@chitkara.edu.in>
2025-05-05 11:39:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Welcome  to React App with CI/CD  Pipelines</h1>
+      <h2>with GITHUB ACTION</h2>
+      <h3>By Nirmit</h3>
+    </div>
+
+
+  )
+}
+
+export default App
